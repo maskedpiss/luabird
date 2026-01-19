68eb11b75c7b5cfac63b8b48925a8810015eae76
maskedpiss <joshkeckedits@proton.me>
2026-01-19 14:21:26 -0600

    drew background

--- src/objs/gameworld.lua
+++ src/objs/gameworld.lua
@@ -1,7 +1,8 @@
 local GameWorld = {}
 
 function GameWorld:load()
-  
+  self.BG = { 0, 0.59, 1 }
+  self.Ground = {}
 end
 
 
@@ -11,7 +12,7 @@ end
 
 
 function GameWorld:draw()
-  
+  love.graphics.setBackgroundColor(self.BG)
 end
 
 return GameWorld
