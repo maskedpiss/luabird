c381c06b8a72784abc1ae40801189b4ed0ec3eab
maskedpiss <joshkeckedits@proton.me>
2026-01-19 13:51:12 -0600

    set up gameworld functions

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
+Globals = {}
+Globals.GameWorld = nil
+
 function love.load()
-  
+  Globals.GameWorld = require("src/objs/gameworld")
+  Globals.GameWorld:load()
 end
 
 
 function love.update(dt)
-  
+  Globals.GameWorld:update(dt)
 end
 
 
 function love.draw()
-  
+  Globals.GameWorld:draw()
 end
---
+++ src/objs/gameworld.lua
@@ -0,0 +1,17 @@
+local GameWorld = {}
+
+function GameWorld:load()
+  
+end
+
+
+function GameWorld:update(dt)
+  
+end
+
+
+function GameWorld:draw()
+  
+end
+
+return GameWorld
