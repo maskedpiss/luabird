75b9c723abd72ddc9372a355e52049d27ef14345
maskedpiss <joshkeckedits@proton.me>
2026-01-19 14:40:13 -0600

    drew ground

--- main.lua
+++ main.lua
@@ -2,6 +2,13 @@ Globals = {}
 Globals.GameWorld = nil
 
 function love.load()
+  Globals.Screen = {
+      x = 0,
+      y = 0,
+      width = love.graphics.getWidth(),
+      height = love.graphics.getHeight()
+  }
+  
   Globals.GameWorld = require("src/objs/gameworld")
   Globals.GameWorld:load()
 end
--- src/objs/gameworld.lua
+++ src/objs/gameworld.lua
@@ -2,7 +2,12 @@ local GameWorld = {}
 
 function GameWorld:load()
   self.BG = { 0, 0.59, 1 }
-  self.Ground = {}
+  self.Ground = {
+      x = Globals.Screen.x,
+      y = Globals.Screen.height - 112,
+      width = Globals.Screen.width,
+      height = Globals.Screen.height
+  }
 end
 
 
@@ -13,6 +18,9 @@ end
 
 function GameWorld:draw()
   love.graphics.setBackgroundColor(self.BG)
+  
+  love.graphics.setColor(0.24, 1, 0.24)
+  love.graphics.rectangle("fill", self.Ground.x, self.Ground.y, self.Ground.width, self.Ground.height)
 end
 
 return GameWorld
