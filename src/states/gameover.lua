b8a9fd4a329810b96fcb633dd4ec1ca7dea9460f
maskedpiss <joshkeckedits@proton.me>
2026-01-21 13:05:10 -0600

    gameover screen now displays player score

--- src/states/gameover.lua
+++ src/states/gameover.lua
@@ -7,6 +7,12 @@ function GameOver.onEnter()
       y = 50,
       text = "Game Over!"
   }
+  
+  GameOver.Score = {
+      font = love.graphics.newFont(64),
+      x = Globals.Screen.x,
+      y = 125
+  }
 end
 
 
@@ -19,6 +25,9 @@ function GameOver.draw()
   love.graphics.setColor(0, 0, 0)
   love.graphics.setFont(GameOver.Message.font)
   love.graphics.printf(GameOver.Message.text, GameOver.Message.x, GameOver.Message.y, Globals.Screen.width, "center")
+  
+  love.graphics.setFont(GameOver.Score.font)
+  love.graphics.printf(Globals.playerScore, GameOver.Score.x, GameOver.Score.y, Globals.Screen.width, "center")
 end
 
 
