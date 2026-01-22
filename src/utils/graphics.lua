b0ba515bde82d5ec43add8d60471802012225035
maskedpiss <joshkeckedits@proton.me>
2026-01-22 15:50:22 -0600

    removed unnecessary colors in graphics util

--- src/utils/graphics.lua
+++ src/utils/graphics.lua
@@ -2,10 +2,6 @@ local Graphics = {}
 
 function Graphics:loadColors()
   self.black = { 0, 0, 0 }
-  self.lightBlue = { 0, 0.59, 1 }
-  self.lightGreen = { 0.24, 1, 0.24 }
-  self.green = { 0, 1, 0 }
-  self.yellow = { 1, 1, 0.39 }
   self.white = { 1, 1, 1 }
 end
 
